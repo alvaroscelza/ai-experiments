30ed5cb5455a41d05cfc2177bb965d42a0af2ffc
Alvaro Scelza <alvaroscelza@gmail.com>
2023-01-14 01:43:50 -0300

    emptied previous rasa project, added action for new experiment.

--- application.py
+++
@@ -1,5 +0,0 @@
-from transformers import pipeline
-
-nlg_pipeline = pipeline("text-generation", model="distilgpt2")
-response = nlg_pipeline("What is the weather like today?")[0]['generated_text']
-print(response)
