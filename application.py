cad42607fa1fe623d6f28c8a4d16aef2046cba3f
Alvaro Scelza <alvaroscelza@gmail.com>
2023-07-13 22:26:34 -0300

    ENtire flow for remote job

--- application.py
+++ application.py
@@ -1,22 +1,84 @@
-import pandas as pd
-from sklearn.tree import DecisionTreeClassifier
+import spacy
+from spacy.cli import download
 
-# Define the dataset
-data = {'is_remote': [1, 1, 0, 0, 0, 1, 0, 1, 1, 0], 'interested': [1, 1, 0, 0, 0, 1, 0, 1, 1, 0]}
-df = pd.DataFrame(data)
 
-# Split the dataset into features and target
-x = df['is_remote']
-x = x.values.reshape(-1, 1)  # reshape the 1 dimension data to convert it into a column.
-y = df['interested']
+class SalaryCalculator:
+    base_price = 5000
+    three_interviews = 1000
+    not_remote_penalty = 1500
+    no_AI_project = 800
+    timezone_specific = 500
+    no_python = 500
+    not_owned_product = 500
+    have_to_use_clients_computer = 400
+    no_english = 300
+    each_additional_interview = 200
+    uninteresting_project = 200
+    cannot_work_on_personal_investigations = 200
 
-# Create a decision tree classifier and fit it to the data
-clf = DecisionTreeClassifier()
-clf.fit(x, y)
+    def calculate_salary(self, features):
+        salary = self.base_price
+        if not features['is_remote_job']:
+            salary += self.not_remote_penalty
+        return salary
 
-# Predict whether a job is of interest or not
-job1 = [[1]]  # remote job
-job2 = [[0]]  # non-remote job
 
-print(clf.predict(job1))
-print(clf.predict(job2))
+class ConversationFilesProcessor:
+    def process_conversation_file(self, file_path):
+        conversation = self.read_conversation(file_path)
+        messages = self.parse_conversation(conversation)
+        return [self.extract_sender_and_message(message) for message in messages]
+
+    @staticmethod
+    def read_conversation(file_path):
+        with open(file_path, 'r', encoding='utf-8') as file:
+            return file.read()
+
+    @staticmethod
+    def parse_conversation(conversation):
+        messages = conversation.split('Sender:')
+        return [message.strip() for message in messages if message.strip()]
+
+    @staticmethod
+    def extract_sender_and_message(message):
+        lines = message.split('\n')
+        sender = lines[0].strip()
+        message = ' '.join(line.strip() for line in lines[2:-1])
+        return {'sender': sender, 'message': message}
+
+
+class SpacyAdapter:
+    model_name = "en_core_web_sm"
+
+    def __init__(self):
+        try:
+            self.natural_language_processor = spacy.load(self.model_name)
+        except OSError:
+            download(self.model_name)
+            self.natural_language_processor = spacy.load(self.model_name)
+
+    def is_remote_job(self, conversation):
+        for pair in conversation:
+            message = pair['message']
+            doc = self.natural_language_processor(message)
+            for token in doc:
+                if token.text.lower() == "remote":
+                    return True
+                if token.text.lower() == "not" and token.head.text.lower() == "remote":
+                    return False
+                if token.text.lower() == "hybrid" and token.head.text.lower() == "job":
+                    return True
+        return False
+
+
+conversations_files_processor = ConversationFilesProcessor()
+file_name = 'conversations_training_data/raw_conversations/Atos-Paula_Calandrelli'
+sender_message_pairs = conversations_files_processor.process_conversation_file(file_name)
+spacy_adapter = SpacyAdapter()
+features = {'is_remote_job': spacy_adapter.is_remote_job(sender_message_pairs)}
+salary_calculator = SalaryCalculator()
+salary = salary_calculator.calculate_salary(features)
+
+print(f"Is Remote Job: {features['is_remote_job']}")
+print(f"Salary: {salary}")
+
